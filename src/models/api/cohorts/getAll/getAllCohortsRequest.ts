832c49ef5a44f4a384fbed66b6f0b5bd891d64e2
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-06-22 16:08:39 +0200

    misc

--- src/models/api/cohorts/getAll/getAllCohortsRequest.ts
+++ src/models/api/cohorts/getAll/getAllCohortsRequest.ts
@@ -1,37 +1,17 @@
-import { object, TypeOf, enum as zenum } from 'zod'
-import { sql } from 'config'
-import { DB_TABLES } from 'constants'
-import { getQueryParams, getSortingOrder, getSortingQuery } from 'helpers'
+import { getQueryParams, getSortingQuery } from 'helpers'
+import { enum as zenum, object, TypeOf } from 'zod'
 import { school, teacher } from '../../../db'
 
 const sortingKeys = zenum(['YEAR', 'IS_CURRENT'])
-const sortingQuery = getSortingQuery(sortingKeys)
-type SortingQuery = TypeOf<typeof sortingQuery>
+const getAllCohortsRequestSorting = getSortingQuery(sortingKeys)
+export type GetAllCohortsRequestSorting = TypeOf<
+  typeof getAllCohortsRequestSorting
+>
 
 const querySchema = object({
   school_id: school.shape.id.optional(),
   teacher_id: teacher.shape.id.optional()
 })
 
-export const queryParams = getQueryParams(querySchema, sortingKeys)
-export type QueryParams = TypeOf<typeof queryParams>
-
-export function getSorting<T extends SortingQuery>(params: T) {
-  const sortBy = params.sort_by
-  const sortOrder = getSortingOrder(params.sort_order)
-
-  switch (sortBy) {
-    default:
-    case 'YEAR':
-      return sql`
-        ORDER BY
-          ${sql(DB_TABLES.COHORTS)}.year ${sortOrder}
-      `
-
-    case 'YEAR':
-      return sql`
-          ORDER BY
-            ${sql(DB_TABLES.COHORTS)}.is_current ${sortOrder}
-        `
-  }
-}
+export const getAllCohortsRequest = getQueryParams(querySchema, sortingKeys)
+export type GetAllCohortsRequest = TypeOf<typeof getAllCohortsRequest>
---
+++ src/models/api/cohorts/getAll/index.ts
@@ -0,0 +1,2 @@
+export * from './getAllCohortsRequest'
+export * from './getAllCohortsResponse'
