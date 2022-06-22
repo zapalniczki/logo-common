4db97c4b2dea8f07af51c30f374c9ecaf8c149e6
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-06-22 21:17:56 +0200

    misc

--- src/helpers/getListResponseBody.ts
+++ src/helpers/getListResponseBody.ts
@@ -1,5 +1,5 @@
 import { array, object, ZodObject, ZodRawShape } from 'zod'
-import { pagination } from 'models'
+import { pagination } from '../models'
 
 function getListResponseBody<T extends ZodRawShape>(schema: ZodObject<T>) {
   const responseBody = object({
