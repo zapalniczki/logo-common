c0dac8d30e9dba4a9679c12e8a2a5f4c7261a16f
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-06-24 17:34:04 +0200

    misc

--- src/models/api/teachers/edit/editTeacherRequest.ts
+++ src/models/api/teachers/edit/editTeacherRequest.ts
@@ -1,12 +1,16 @@
 import { TypeOf } from 'zod'
 import { teacher } from '../../../db'
 
-export const editTeacherRequest = teacher.omit({
-  created_at: true,
-  updated_at: true,
-  school_id: true,
-  email_confirmed: true,
-  blocked: true
-})
+export const editTeacherRequest = teacher
+  .omit({
+    created_at: true,
+    updated_at: true,
+    school_id: true,
+    email_confirmed: true,
+    blocked: true,
+    id: true
+  })
+  .partial()
+  .merge(teacher.pick({ id: true }))
 
 export type EditTeacherRequest = TypeOf<typeof editTeacherRequest>
