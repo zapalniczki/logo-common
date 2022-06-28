ae9f0d197e53f9a71cb9b41c932bb3774b4eae11
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-06-28 20:30:14 +0200

    misc

--- src/models/api/cohorts/add/addCohortRequest.ts
+++ src/models/api/cohorts/add/addCohortRequest.ts
@@ -1,8 +1,9 @@
 import { object, TypeOf } from 'zod'
-import { cohort } from '../../../db'
+import { cohort, school } from '../../../db'
 
 export const addCohortRequest = object({
-  year: cohort.shape.year
+  year: cohort.shape.year,
+  school_id: school.shape.id
 })
 
 export type AddCohortRequest = TypeOf<typeof addCohortRequest>
