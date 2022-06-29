717b75306a1066635104878ab34c8b0e41d4818c
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-06-29 16:27:21 +0200

    typo

--- src/models/api/teachers/delete/deleteTeacherRequest.ts
+++ src/models/api/teachers/delete/deleteTeacherRequest.ts
@@ -1,8 +1,8 @@
 import { object, TypeOf } from 'zod'
 import { cohort, teacher } from '../../../db'
 
-export const teacherCohortRequest = object({
+export const teacherDeleteRequest = object({
   teacher_id: teacher.shape.id
 })
 
-export type TeacherCohortRequest = TypeOf<typeof teacherCohortRequest>
+export type TeacherDeleteRequest = TypeOf<typeof teacherDeleteRequest>
