7542df5b3a5ac82f65184d1d65210130edcca15e
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-06-29 16:09:31 +0200

    misc

--- src/models/api/teachers/getDetails/getTeacherDetailsResponse.ts
+++ src/models/api/teachers/getDetails/getTeacherDetailsResponse.ts
@@ -1,4 +1,4 @@
-import { TypeOf, enum as zenum, array } from 'zod'
+import { array, boolean, enum as zenum, TypeOf } from 'zod'
 import { teacher } from '../../../db'
 
 const getTeacherDetailsResponsePermissions = zenum([
@@ -13,14 +13,16 @@ export type GetTeacherDetailsResponsePermissions = TypeOf<
   typeof getTeacherDetailsResponsePermissions
 >
 
-export const getTeacherDetailsResponseSchema = teacher.pick({
-  name: true,
-  email: true,
-  id: true,
-  surname: true,
-  email_confirmed: true,
-  blocked: true
-})
+export const getTeacherDetailsResponseSchema = teacher
+  .pick({
+    name: true,
+    email: true,
+    id: true,
+    surname: true,
+    email_confirmed: true,
+    blocked: true
+  })
+  .extend({ has_quiz_assignments: boolean() })
 
 export const getTeacherDetailsResponse = getTeacherDetailsResponseSchema.extend(
   {
