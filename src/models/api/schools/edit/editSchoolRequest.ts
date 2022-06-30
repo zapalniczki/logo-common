7080900fdeeab16775f0f00ff96bc7035a03afea
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-06-30 23:13:59 +0200

    'misc'

---
+++ src/models/api/schools/edit/editSchoolRequest.ts
@@ -0,0 +1,16 @@
+import { TypeOf } from 'zod'
+import { school } from '../../../db'
+
+export const editSchoolRequest = school
+  .omit({
+    created_at: true,
+    updated_at: true,
+    school_id: true,
+    email_confirmed: true,
+    admin_id: true,
+    id: true
+  })
+  .partial()
+  .merge(school.pick({ id: true }))
+
+export type EditTeacherRequest = TypeOf<typeof editSchoolRequest>
--- src/models/api/schools/index.ts
+++ src/models/api/schools/index.ts
@@ -1,3 +1,4 @@
 export * from './getDetails'
 export * from './getAll'
 export * from './add'
+export * from './edit'
