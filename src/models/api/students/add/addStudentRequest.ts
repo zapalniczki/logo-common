d88d1039473bfba0cd43c659f124e0d63a1a2c58
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-07-01 20:49:52 +0200

    'misc'

--- src/models/api/students/add/addStudentRequest.ts
+++ src/models/api/students/add/addStudentRequest.ts
@@ -1,11 +1,12 @@
 import { TypeOf } from 'zod'
 import { student } from '../../../db'
 
-export const addStudentRequest = student.omit({
-  created_at: true,
-  updated_at: true,
-  id: true,
-  email_confirmed: true
+export const addStudentRequest = student.pick({
+  name: true,
+  surname: true,
+  email: true,
+  group_id: true,
+  school_id: true
 })
 
 export type AddStudentRequest = TypeOf<typeof addStudentRequest>
