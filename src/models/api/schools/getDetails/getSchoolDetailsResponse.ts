571e2fae033af9f284d9abf4d343a8a910de3e0a
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-07-01 23:29:24 +0200

    'misc'

--- src/models/api/schools/getDetails/getSchoolDetailsResponse.ts
+++ src/models/api/schools/getDetails/getSchoolDetailsResponse.ts
@@ -4,7 +4,9 @@ import { school } from '../../../db'
 export const getSchoolDetailsResponse = school.pick({
   name: true,
   email: true,
-  id: true
+  id: true,
+  blocked: true,
+  email_confirmed: true
 })
 
 export type GetSchoolDetailsResponse = TypeOf<typeof getSchoolDetailsResponse>
