2a2f5634a70784285e34f6f68a4c0239aceb24a4
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-07-02 12:10:20 +0200

    'misc'

--- src/models/api/schools/getDetails/getSchoolDetailsResponse.ts
+++ src/models/api/schools/getDetails/getSchoolDetailsResponse.ts
@@ -1,12 +1,42 @@
-import { TypeOf } from 'zod'
+import { TypeOf, enum as zenum, array, boolean, object } from 'zod'
 import { school } from '../../../db'
 
-export const getSchoolDetailsResponse = school.pick({
-  name: true,
-  email: true,
-  id: true,
-  blocked: true,
-  email_confirmed: true
-})
+const getSchoolDetailsResponsePermission = zenum([
+  'DELETE',
+  'BLOCK',
+  'UNBLOCK',
+  'EDIT',
+  'SEND_ACTIVATION'
+])
+
+export type GetSchoolDetailsResponsePermission = TypeOf<
+  typeof getSchoolDetailsResponsePermission
+>
+
+export const getSchoolDetailsResponseSchema = school
+  .pick({
+    name: true,
+    email: true,
+    id: true,
+    blocked: true,
+    email_confirmed: true
+  })
+  .extend({
+    has_cohorts: boolean(),
+    has_groups: boolean(),
+    has_teachers: boolean()
+  })
+
+export type GetSchoolDetailsResponseSchema = TypeOf<
+  typeof getSchoolDetailsResponseSchema
+>
+
+export const getSchoolDetailsResponse = getSchoolDetailsResponseSchema
+  .omit({
+    has_cohorts: true,
+    has_groups: true,
+    has_teachers: true
+  })
+  .extend({ permissions: array(getSchoolDetailsResponsePermission) })
 
 export type GetSchoolDetailsResponse = TypeOf<typeof getSchoolDetailsResponse>
