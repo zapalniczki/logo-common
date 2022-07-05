f5888bc6c154874f269aa7eb43a72c0d0be652b6
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-07-05 21:47:44 +0200

    'misc'

--- src/models/api/teachers/getAll/getAllTeachersRequest.ts
+++ src/models/api/teachers/getAll/getAllTeachersRequest.ts
@@ -2,13 +2,30 @@ import { enum as zenum, object, TypeOf } from 'zod'
 import { getQueryParams, getSortingQuery } from '../../../../helpers'
 import { school } from '../../../db'
 
-const sortingKeys = zenum(['SURNAME', 'EMAIL', 'EMAIL_CONFIRMED', 'CREATED_AT'])
-const sortingQuery = getSortingQuery(sortingKeys)
-type SortingQuery = TypeOf<typeof sortingQuery>
+export const getAllTeachersRequestSortingKeys = zenum([
+  'SURNAME',
+  'EMAIL',
+  'EMAIL_CONFIRMED',
+  'CREATED_AT'
+])
 
-export const querySchema = object({
+export type GetAllTeachersRequestSortingKeys = TypeOf<
+  typeof getAllTeachersRequestSortingKeys
+>
+
+export const getAllTeachersRequestSortingQuery = getSortingQuery(
+  getAllTeachersRequestSortingKeys
+)
+export type GetAllTeachersRequestSortingQuery = TypeOf<
+  typeof getAllTeachersRequestSortingQuery
+>
+
+export const getAllTeachersRequestSchema = object({
   school_id: school.shape.id
 })
 
-export const getAllTeachersRequest = getQueryParams(querySchema, sortingKeys)
+export const getAllTeachersRequest = getQueryParams(
+  getAllTeachersRequestSchema,
+  getAllTeachersRequestSortingKeys
+)
 export type GetAllTeachersRequest = TypeOf<typeof getAllTeachersRequest>
