2e0f378d9a3da0ab1c9cdbfb019daec1e5bf7451
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-07-06 20:43:56 +0200

    'misc'

--- src/models/api/statistics/getSubmissions/getStatisticsSubmissionsRequest.ts
+++ src/models/api/statistics/getSubmissions/getStatisticsSubmissionsRequest.ts
@@ -2,9 +2,19 @@ import { getQueryParams, getSortingQuery } from '../../../../helpers'
 import { enum as zenum, object, TypeOf } from 'zod'
 import { student, teacher } from '../../../db'
 
-const sortingKeys = zenum(['COMPLETED_AT'])
-const sortingQuery = getSortingQuery(sortingKeys)
-type SortingQuery = TypeOf<typeof sortingQuery>
+export const getStatisticsSubmissionsRequestSortingKeys = zenum([
+  'COMPLETED_AT'
+])
+export type GetStatisticsSubmissionsRequestSortingKeys = TypeOf<
+  typeof getStatisticsSubmissionsRequestSortingKeys
+>
+
+export const getStatisticsSubmissionsRequestSortingQuery = getSortingQuery(
+  getStatisticsSubmissionsRequestSortingKeys
+)
+export type GetStatisticsSubmissionsRequestSortingQuery = TypeOf<
+  typeof getStatisticsSubmissionsRequestSortingQuery
+>
 
 const querySchema = object({
   teacher_id: teacher.shape.id.optional(),
@@ -13,7 +23,7 @@ const querySchema = object({
 
 export const getStatisticsSubmissionsRequest = getQueryParams(
   querySchema,
-  sortingKeys
+  getStatisticsSubmissionsRequestSortingKeys
 )
 export type GetStatisticsSubmissionsRequest = TypeOf<
   typeof getStatisticsSubmissionsRequest
