21f6060502b9892f2c597cfd8dbf0b3181b7106b
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-07-06 23:06:52 +0200

    'misc'

---
+++ src/constants/endpoints/FUN_FACTS.ts
@@ -0,0 +1,5 @@
+const FUN_FACTS = {
+  getContent: '/Admins/getContent'
+} as const
+
+export default FUN_FACTS
--- src/constants/endpoints/index.ts
+++ src/constants/endpoints/index.ts
@@ -1,22 +1,24 @@
 import ADMINS from './ADMINS'
 import AUTH from './AUTH'
 import COHORTS from './COHORTS'
+import FUN_FACTS from './FUN_FACTS'
 import GROUPS from './GROUPS'
 import KPIS from './KPIS'
+import QUIZES from './QUIZES'
 import QUIZ_ASSIGNMENTS from './QUIZ_ASSIGNMENTS'
 import QUIZ_ATTEMPTS from './QUIZ_ATTEMPTS'
 import QUIZ_INSTANCES from './QUIZ_INSTANCES'
 import SCHOOLS from './SCHOOLS'
 import STATISTICS from './STATISTICS'
+import STUDENTS from './STUDENTS'
 import TEACHERS from './TEACHERS'
 import TEMP from './TEMP'
-import QUIZES from './QUIZES'
-import STUDENTS from './STUDENTS'
 
 const ENDPOINTS = {
   ADMINS,
   AUTH,
   COHORTS,
+  FUN_FACTS,
   GROUPS,
   KPIS,
   QUIZES,
