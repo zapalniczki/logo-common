4c3ff8c1fc12f2632120472d9b6402e34e48f32a
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-07-09 21:42:07 +0200

    'misc'

--- src/models/api/students/getDetails/getStudentDetailsRequest.ts
+++ src/models/api/students/getDetails/getStudentDetailsRequest.ts
@@ -1,8 +1,20 @@
-import { object, TypeOf } from 'zod'
-import { student } from '../../../db'
+import { object, TypeOf, undefined, intersection, union } from 'zod'
+import { school, student, teacher } from '../../../db'
 
-export const getStudentDetailsRequest = object({
+export const schema = object({
   student_id: student.shape.id
 })
+
+const teacherSchema = object({
+  teacher_id: teacher.shape.id,
+  school_id: undefined()
+}).merge(schema)
+
+const schoolSchema = object({
+  teacher_id: undefined(),
+  school_id: school.shape.id
+}).merge(schema)
+
+export const getStudentDetailsRequest = union([teacherSchema, schoolSchema])
 
 export type GetStudentDetailsRequest = TypeOf<typeof getStudentDetailsRequest>
