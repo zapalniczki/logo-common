a4d832a5df7f8d400b460853bfe4b83d7df284e6
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-07-10 00:58:09 +0200

    'misc'

--- src/models/api/quizes/getDetails/getQuizDetailsResponse.ts
+++ src/models/api/quizes/getDetails/getQuizDetailsResponse.ts
@@ -1,11 +1,29 @@
-import { TypeOf } from 'zod'
+import { TypeOf, enum as zenum, array } from 'zod'
 import { quiz } from '../../../db'
 
-export const getQuizDetailsResponse = quiz.pick({
+const getQuizDetailsResponsePermission = zenum([
+  'VIEW_QUESTIONS',
+  'VIEW_VERSIONS',
+  'USE'
+])
+
+export type GetQuizDetailsResponsePermission = TypeOf<
+  typeof getQuizDetailsResponsePermission
+>
+
+export const getQuizDetailsResponseSchema = quiz.pick({
   name: true,
   introduction: true,
   category: true,
   id: true
+})
+
+export type GetQuizDetailsResponseSchema = TypeOf<
+  typeof getQuizDetailsResponseSchema
+>
+
+export const getQuizDetailsResponse = getQuizDetailsResponseSchema.extend({
+  permissions: array(getQuizDetailsResponsePermission)
 })
 
 export type GetQuizDetailsResponse = TypeOf<typeof getQuizDetailsResponse>
