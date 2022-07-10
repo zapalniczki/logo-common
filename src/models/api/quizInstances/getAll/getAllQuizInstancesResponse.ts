5b76120ce6f76d77b44cd3edc030dad47c5fd670
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-07-10 11:26:13 +0200

    'misc'

--- src/models/api/quizInstances/getAll/getAllQuizInstancesResponse.ts
+++ src/models/api/quizInstances/getAll/getAllQuizInstancesResponse.ts
@@ -1,6 +1,12 @@
-import { number, object, TypeOf } from 'zod'
+import { number, object, TypeOf, enum as zenum, array } from 'zod'
 import { getListResponseBody } from '../../../../helpers'
 import { quizInstance } from '../../../db'
+
+const getAllQuizInstancesResponsePermission = zenum(['ADD'])
+
+export type GetAllQuizInstancesResponsePermission = TypeOf<
+  typeof getAllQuizInstancesResponsePermission
+>
 
 export const getAllQuizInstancesResponseSchema = object({
   id: quizInstance.shape.id,
@@ -12,7 +18,7 @@ export const getAllQuizInstancesResponseSchema = object({
 
 export const getAllQuizInstancesResponse = getListResponseBody(
   getAllQuizInstancesResponseSchema
-)
+).extend({ permissions: array(getAllQuizInstancesResponsePermission) })
 
 export type GetAllQuizInstancesResponse = TypeOf<
   typeof getAllQuizInstancesResponse
