1ca741165723d603afcb6f6fecd8fb0beb271d49
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-07-26 21:55:50 +0200

    'misc'

--- src/models/api/quizAssignments/getDetails/getQuizAssignmentDetailsRequest.ts
+++ src/models/api/quizAssignments/getDetails/getQuizAssignmentDetailsRequest.ts
@@ -1,9 +1,10 @@
 import { object, TypeOf } from 'zod'
+import { getUserSchema } from '../../../../helpers'
 import { quizAssignment } from '../../../db'
 
-export const getQuizAssignmentDetailsRequest = object({
-  quiz_assignment_id: quizAssignment.shape.id
-})
+export const getQuizAssignmentDetailsRequest = getUserSchema(
+  object({ quiz_assignment_id: quizAssignment.shape.id })
+)
 
 export type GetQuizAssignmentDetailsRequest = TypeOf<
   typeof getQuizAssignmentDetailsRequest
