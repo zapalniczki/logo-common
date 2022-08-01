b8aa15143beec7c3acf171cdebe0ba7465e6f88d
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-08-01 20:38:04 +0200

    'misc'

--- src/constants/endpoints/QUIZ_ASSIGNMENTS.ts
+++ src/constants/endpoints/QUIZ_ASSIGNMENTS.ts
@@ -1,5 +1,6 @@
 const QUIZ_ASSIGNMENTS = {
   add: '/quiz-assignments/add',
+  edit: '/quiz-assignments/edit',
   getAll: '/quiz-assignments/getAll',
   getDetails: '/quiz-assignments/getDetails',
   getProgress: '/quiz-assignments/getProgress'
